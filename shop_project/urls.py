d92214633bc75c19785a75453c1d73fa63222d32
Tanya-888 <vileyka.box@gmail.com>
2026-03-20 11:23:53 +0300

    pj

--- shop_project/urls.py
+++ shop_project/urls.py
@@ -16,12 +16,22 @@ Including another URLconf
 """
 
 
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
 from django.views.generic import RedirectView
+
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('api/', include('shop.urls')),
 
     path('', RedirectView.as_view(url='/api/', permanent=False)),
